af18e99a228e566859a165349a046fef5fb1c7e2
Devender Shakya <devshakya@gmail.com>
2023-08-08 01:28:06 +0530

    Homepage 60% Complete

--- component/navbar.js
+++ component/navbar.js
@@ -1,21 +1,4 @@
 function navbar(){
-    return `      <a href="#"><img src="image/navbar_logo.jpg" class="logo" alt="" /></a>
-    <div class="form">
-        <input type="text" placeholder="Search any items" />
-        <button class="normal">Search</button>
-    </div>
-    <div>
-        <ul id="navbar">
-            <li><a class="active" href="index.html">Home</a></li>
-            <li><a href="sign up.html">Sign up</a></li>
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li id="lg-bag">
-                <a href="cart.html"><i class="fa-solid fa-cart-shopping fa-bounce" style="color: #dfab62;"></i></a>
-                <a href="#" id="close"><i class="fa-regular fa-circle-xmark"></i></a>
-            </li>
-        </ul>
-    </div>`
+    return `      `
 }
 export default navbar;
